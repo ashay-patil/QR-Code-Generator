f896ffcc59d49597cc93b4275435ba1f20557d57
Ashay Patil <ashaypatil2005@gmail.com>
2025-01-30 01:30:51 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const btn = document.getElementsByTagName('button')[0];
+const imgbox = document.getElementById('imgBox');
+const qrImage = document.getElementById('Qr-Image');
+const input = document.getElementById('Qr-text');
+const loadelement = document.createElement('p');
+imgbox.append(loadelement);
+const getQR = async ()=>{
+    loadelement.hidden = false;
+    loadelement.classList.toggle('invalid-input');
+    if(input.value.length > 0){
+    qrImage.src = '';
+    loadelement.innerHTML = 'Loading ...';
+    const response = await fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`)
+    qrImage.src = response.url;
+    loadelement.hidden = true;
+    }
+    else{
+        loadelement.classList.add('invalid-input');
+        loadelement.innerHTML = 'Invalid Input !';
+    }
+}
+
+btn.addEventListener('click',getQR);
